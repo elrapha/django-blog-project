dc81f8cf669f9c073cc71e67fe3300dbd6ad2b1f
elrapha <elrapha@elrapha-pc.(none)>
2012-07-06 12:56:24 +0000

    updated blog

--- blog/models.py
+++ blog/models.py
@@ -3,16 +3,16 @@ from django.db import models
 class Post(models.Model):
     title=models.CharField(max_length=60)
     body=models.TextField()
-    created=models.DateField()
-    updated=models.DateField()
+    created=models.DateField(auto_now_add=True)
+    updated=models.DateField(auto_now=True)
     def __unicode__(self):
         return self.title
     
 class Comment(models.Model):
     body=models.TextField()
     author=models.CharField(max_length=60)
-    created=models.DateField()
-    updated=models.DateField()
+    created=models.DateField(auto_now_add=True)
+    updated=models.DateField(auto_now=True)
     post=models.ForeignKey(Post)
     def __unicode__(self):
         return self.author
