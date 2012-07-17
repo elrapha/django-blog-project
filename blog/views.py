6caa0ea9f5fac7cc05c00e8f603fdc4c3f64ef83
elrapha <elrapha@elrapha-pc.(none)>
2012-07-17 10:55:58 +0000

    yblog

--- blog/views.py
+++ blog/views.py
@@ -11,67 +11,87 @@ from django.shortcuts import render_to_response
 from models import Post, Comment 
 from django.forms import ModelForm
 from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect,HttpResponseForbidden
 
+from reg.views import LoginForm
 
 def post_list(request):
-    list_of_post= Post.objects.all()
-    
+    logged_in =request.user.is_authenticated() # get login status
+    list_of_post= Post.objects.all() # get all posts
+    current_user=str(request.user) # get current user's name
     #return HttpResponse('This should be a list of posts!')
-    return render_to_response('post_list.html',locals())
+    return render_to_response('blog/post_list.html',locals()) #return
 
-class CommentForm(ModelForm):
-	class Meta:
-		model=Comment
-		exclude=['post']
+class CommentForm(ModelForm): # create a class that mimics the Comment class in models.py
+    class Meta:
+        model=Comment # make class of the same model as Comment in models.py
+        exclude=['post','author'] # remove post and author fields from the form
 		
 @csrf_exempt
 def post_detail(request, id, showComments):
-    POST=Post.objects.get(id=id)
-    if request.method == 'POST':
-        comment = Comment(post=POST)
-        form = CommentForm(request.POST,instance=comment)
-        if form.is_valid():
-            form.save()
-        return HttpResponseRedirect(request.path)
+    logged_in =request.user.is_authenticated()# get login status
+    trueval="true"
+    if request.user.is_authenticated(): #if user is authenticated
+        P=Post.objects.get(id=id) # get post by id
+        current_user=str(request.user) # get username as string
+        if request.method == 'POST': # if user clicks submit button do...
+            comment = Comment(post=P,author=current_user)# create instance of Comment with post and author attributes initialized
+            form = CommentForm(request.POST,instance=comment)# get form field inputs
+            if form.is_valid():#if form is valid
+                form.save()# save in database
+            return HttpResponseRedirect(request.path)
+        else:
+            form = CommentForm() # instance of empty form
+
+        
+        title=P.title
+        body=P.body
+        created=P.created
+        updated=P.updated
+        comments=''
+        if showComments=="true":
+            comments=P.comment_set.all()# get all comments associated with post P
+
+        
+        return render_to_response('blog/post_details.html',locals())
     else:
-        form = CommentForm()
-
-    
-    title=POST.title
-    body=POST.body
-    created=POST.created
-    updated=POST.updated
-    comments=''
-    if showComments!=None:
-        comments=POST.comment_set.all()
-
-    return render_to_response('post_details.html',locals())
-
+        return HttpResponseRedirect('/reg/login/')
+        
 @csrf_exempt
 def edit_comment(request,comID):
-    comment=Comment.objects.get(id=comID)
-    Related_Post=comment.post
-    POST=Post.objects.get(pk=Related_Post.id)
-    if request.method == 'POST':
-        form = CommentForm(request.POST,instance=Comment.objects.get(id=comID))
-        if form.is_valid():
-            form.save()
-        return HttpResponseRedirect(POST.get_absolute_url())
+    logged_in =request.user.is_authenticated() # get login status
+    current_user=request.user #get current user
+    comment=Comment.objects.get(id=comID) # get comment by id
+    Related_Post=comment.post # get the related post of the comment
+    P=Post.objects.get(pk=Related_Post.id)# get post 
+    if request.method == 'POST': # if user clicks submit
+        if str(current_user) == comment.author:
+            form = CommentForm(request.POST,instance=Comment.objects.get(id=comID))
+            if form.is_valid():
+                form.save() # save in database
+            return HttpResponseRedirect(P.get_absolute_url())
+        else:
+            return HttpResponseForbidden('Sorry. You are not allowed to edit this comment')
     else:
         form = CommentForm(instance=Comment.objects.get(id=comID))
         
-    return render_to_response('edit_comment.html',locals())
+    return render_to_response('blog/edit_comment.html',locals())
+
 
 def post_search(request, term):
-    keyword=term
-    list_of_post = Post.objects.filter(body__icontains=term)
-    return render_to_response('post_search.html',locals())
-
+    logged_in =request.user.is_authenticated()# get login status
+    current_user=str(request.user) # get current user
+    if request.user.is_authenticated():
+        keyword=term
+        list_of_post = Post.objects.filter(body__icontains=term)# check if body of bost contains search term
+        return render_to_response('blog/post_search.html',locals())
+    else:
+        return HttpResponseRedirect('/reg/login/')
 def home(request):
-    #print 'it works'
-    #return HttpResponse('hello world. Ete zene?')
-    return render_to_response('post_home.html','') 
+    current_user=str(request.user)# get current user
+    logged_in =request.user.is_authenticated()# get login status
+    
+    return render_to_response('blog/post_home.html',locals()) 
 
 
 
--- myblog/urls.py
+++ myblog/urls.py
@@ -8,6 +8,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
 url(r'^admin/', include(admin.site.urls)),
 url(r'^blog/', include('blog.urls')),
+url(r'^reg/', include('reg.urls')),
 url(r'^static/(?P<path>.*)$', 'django.views.static.serve', 
 {'document_root' : settings.STATIC_ROOT,}),
 
